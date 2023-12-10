26e1cfff0ca75e56ba392808d3a4019050bb9de6
aszweryn <101809061+aszweryn@users.noreply.github.com>
2023-12-10 16:37:21 +0100

    feat: return request path

--- src/backend.lua
+++ src/backend.lua
@@ -4,7 +4,8 @@ backend.generate_traffic = function()
   ngx.header['Content-Type'] = 'application/json'
   ngx.header['Cache-Control'] = 'public, max-age=' .. (ngx.var.arg_max_age or 15)
 
-  ngx.say('{"service": "api", "value": 123}')
+  -- returns request path
+  ngx.say('{"service": "api", "value": 123, "request": "' .. ngx.var.uri .. '"}')
 end
 
 return backend
