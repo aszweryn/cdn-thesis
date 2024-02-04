36ffba3e2d61231ba772dd0059dd92e417132c1f
aszweryn <101809061+aszweryn@users.noreply.github.com>
2024-02-04 15:45:26 +0100

    test: changing value of latency for grafana dashboard building experiments

--- src/backend.lua
+++ src/backend.lua
@@ -5,7 +5,7 @@ backend.generate_traffic = function()
   simulation.for_work_longtail(simulation.profiles.backend)
 
   ngx.header['Content-Type'] = 'application/json'
-  ngx.header['Cache-Control'] = 'public, max-age=' .. (ngx.var.arg_max_age or 15)
+  ngx.header['Cache-Control'] = 'public, max-age=' .. (ngx.var.arg_max_age or 10)
 
   ngx.say('{"service": "api", "value": 123, "request": "' .. ngx.var.uri .. '"}')
 end
