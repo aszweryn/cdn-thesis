d16937eeacec299caeb8ad9cec164c73b6160670
aszweryn <101809061+aszweryn@users.noreply.github.com>
2024-02-08 17:06:04 +0100

    fix: generic 200 as sucessfullr response code

--- src/backend.lua
+++ src/backend.lua
@@ -7,7 +7,7 @@ backend.generate_traffic = function()
   ngx.header['Content-Type'] = 'application/json'
   ngx.header['Cache-Control'] = 'public, max-age=' .. (ngx.var.arg_max_age or 10)
 
-  ngx.say('{"service": "api", "value": 123, "request": "' .. ngx.var.uri .. '"}')
+  ngx.say('{"service": "api", "value": 200, "request": "' .. ngx.var.uri .. '"}')
 end
 
 return backend
