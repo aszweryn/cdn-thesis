8610b69209aad2842191a1d3bbde17126dcd7256
aszweryn <101809061+aszweryn@users.noreply.github.com>
2024-03-14 19:18:03 +0100

    feat: scripts for load testing both scenarios

---
+++ load-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+trap "echo -e '\n\e[31mABORTED!\e[0m'" INT
+
+echo -e "\e[32mLoad testing the CDN version of the application\e[0m"
+
+output=$(wrk \
+  --connections 10 \
+  --threads 2 \
+  --duration 1s \
+  --script ./src/load-tests.lua \
+  --latency \
+  http://localhost:18080)
+
+echo -e "\e[96m${output}\e[0m"
