c19c42db406a0bd56920d6109997889ce7754533
aszweryn <101809061+aszweryn@users.noreply.github.com>
2024-05-16 19:08:20 +0200

    feat: new latency profiles, minor fixes

--- src/backend.lua
+++ src/backend.lua
@@ -3,29 +3,9 @@ local cjson = require "cjson.safe"
 
 local backend = {}
 
--- Generate traffic with a long-tail distribution and send a JSON response
+-- Generate traffic with a long-tail distribution
 backend.generate_traffic = function()
   simulation.simulateLongTail(simulation.profiles.backend)
-
-  -- Set the response headers
-  ngx.header['Content-Type'] = 'application/json'
-  ngx.header['Cache-Control'] = 'public, max-age=' .. (ngx.var.arg_max_age or 10)
-
-  -- Prepare the response body
-  local response_body = {
-    service = "api",
-    value = 200,
-    request = ngx.var.uri
-  }
-
-  -- Encode the response as JSON and send it
-  local json_response, err = cjson.encode(response_body)
-  if not json_response then
-    ngx.log(ngx.ERR, "Failed to encode the response body: ", err)
-    return
-  end
-
-  ngx.say(json_response)
 end
 
 return backend
